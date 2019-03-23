f42c7c719fcb5301262570dfd70b6f7fc505024b
jffersn <32963293+jffersn@users.noreply.github.com>
2019-03-23 00:13:35 -0400

    fak

---
+++ js/instructorFB.js
@@ -0,0 +1,8 @@
+
+function loadFeedBack(){
+    for(let i = 0; i<11;i++){// loop till database is empty??? 
+        var div = document.createElement('p');
+        div.innerHTML = i +"   blah blah sample lorem ";//put contents of feedback here
+        document.getElementById('feedbacks').prepend(div);
+    }
+}
