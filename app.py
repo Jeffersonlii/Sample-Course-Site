e49b09f3d571eb78d4c124893b5124f024126d4e
jffersn <32963293+jffersn@users.noreply.github.com>
2019-03-29 08:43:57 -0400

    solved most tech debt in .py

--- app.py
+++ app.py
@@ -9,17 +9,18 @@ db = SQLAlchemy(app)
 @app.route('/',methods=['GET','POST'])
 @app.route('/login',methods=['GET','POST'])
 def login():
-    if 'email' in session:
-        data = getData(session['email'])
-        return query(data)
+
+    if 'name' in session:
+        return redirect(url_for('home')) 
 
     elif request.method=='POST':
         results = db.engine.execute(text("select * from users"))
         for result in results: 
             if result['email'] == request.form['email']:
                 if result['password'] == request.form['password']:
-                    return query(result)
-                else:
+                    fillSession(result)
+                    return redirect(url_for('home')) 
+                else: # if pass isnt correct, go back
                     return render_template('index.html')
 
         return render_template('index.html')
@@ -27,91 +28,107 @@ def login():
     elif request.method=='GET':
         return render_template('index.html')
 
-def query(result):
-    data = {}
-    if result['type'] == "INSTRUCTOR":
-        sql = """
-            select name,reviews from users 
-            left join feedback on users.email == feedback.email
-            where users.email == {}""".format(result['email'])
+def fillSession(result):
+    if 'email' not in session:
         session['email'] = result['email']
         session['typ'] = result['type']
-        #returns instructor name and reviews
-        data = db.engine.execute(text(sql))
+        session['name'] = result['name']
 
-    else:# if user is a student
-        sql = """
+
+def query(requested_data):
+    # returns bundle of requested data
+    # could request: 
+    # instructor: feedback, all student grades
+    # students : students grades
+    data = []# package of data depending on requested
+    if session['typ'] == 'INSTRUCTOR':
+        if requested_data == "FEEDBACK":
+            sql = """
+            select name,reviews from users 
+            left join feedback on users.email == feedback.email
+            where users.email == {}""".format(session['email'])
+            data = db.engine.execute(text(sql))
+        elif requested_data == "ALLGRADES":#requesting all student grades
+            data = db.engine.execute(text('select * from grades'))
+
+    else:# else, its a student requesting his grades
+        if requested_data == "MYGRADES":
+            sql = """
             select name, a1,a2,a3,q1,q2,q3,q4,midterm,final from users 
             left join grades 
             on users.studentNumber == grades.studentNumber
             where type != "INSTRUCTOR" and users.email =={}
-            """.format(result['email'])
-        session['email'] = result['email']
-        session['typ'] = result['type']
-        data = db.engine.execute(text(sql))
-    
-    return goToHome(data)
+            """.format(session['email'])
+            data = db.engine.execute(text(sql))
+    return data
 
-def getData(email):
-    results = db.engine.execute(text("select * from users"))
-    for result in results: 
-        if result['email'] == email:
-            return result
+    
+
 
 ######similar pages####### V
 @app.route('/CWork')
 def a():
-    return render_template('CWork.html')
+    return render_template('CWork.html', data= session['name'])
 
 @app.route('/instructor')
 def b():
-    return render_template('instructor.html')
+    return render_template('instructor.html', data= session['name'])
 
 @app.route('/Ohour')
 def c():
-    return render_template('Ohour.html')
+    return render_template('Ohour.html', data= session['name'])
 
 #####instructor pages##### V
-@app.route('/myFeedBack')
+@app.route('/myFeedBack')#need query info
 def d():
-    return render_template('myFeedBack.html')
+    lstF = []
+    data = query("FEEDBACK")
+    for i in data:
+        fbs = i['reviews']
+        # review will look like 'this is a review $# this is another $#'
+        start = 0
+        for end in range(1,len(fbs)):
+            if fbs[end] == '#' and fbs[end - 1] == '$':
+                lstF.append(fbs[start:end - 1])
+                start = end + 1
+        return render_template('myFeedBack.html', data=lstF, name = session['name'])
 
-@app.route('/gradesAll')
+@app.route('/gradesAll')#need query info
 def f():
-    return render_template('gradesAll.html')
+    return render_template('gradesAll.html', data=query("ALLGRADES"), name = session['name'])
 
 #####student pages##### V
-@app.route('/Feedback')
+@app.route('/Feedback') 
 def g():
-    return render_template('Feedback.html')
+    return render_template('Feedback.html', data= session['name'])
 
-@app.route('/gradesMy')
+@app.route('/gradesMy')#need query info
 def i():
-    return render_template('gradesMy.html')
+    data = query("MYGRADES")
+    for i in data:
+        return render_template('gradesMy.html', data=i)
 
 ##### home ####### V
 @app.route('/home')
-def goToHome(data = None):
-    if data:
-        if session['typ'] == 'INSTRUCTOR':
-            return render_template('homeInstructor.html',data=data)
-        else:
-            return render_template('homeStudent.html',data=data)
-    else: 
-        data = getData(session['email'])
-        return query(data)
+def home():
+    if session['typ'] == 'INSTRUCTOR':
+        return render_template('homeInstructor.html', data= session['name'])
+    else:
+        return render_template('homeStudent.html', data = session['name'])
 
 ######log out############
 @app.route('/logout')
 def logout():
-    session.pop('email')
-    session.pop('typ')
-    return redirect(url_for('login'))
+    if 'name' in session:
+        session.pop('name')
+    
+    if 'email' in session:
+        session.pop('email')
 
-######pdfs#############
-@app.route('/syl')
-def syl():
-    return render_template('assets/syl.pdf')
+    if 'typ' in session:
+        session.pop('typ')
+    
+    return redirect(url_for('login')) 
 
 if __name__ =="__main__":
     app.run(debug=True)
