115fa4303fbfcfcdab2374c6c71ac22bf9c3b42f
Jefferson(Zhe Fan) Li <32963293+Jeffersonlii@users.noreply.github.com>
2019-04-05 09:31:54 -0400

    Add files via upload

---
+++ Assignment3/static/js/login.js
@@ -0,0 +1,48 @@
+$(document).ready(function(){
+
+
+	//reg button
+	$('#register').click(function(){
+		$('.popUpReg').css('display', 'flex');
+	});
+	$('#register').hover(function(){
+		$('#register').css("background","Silver");
+	},function(){
+		$('#register').css("background","white");
+	})
+	//close popup button
+	$('#close').click(function(){
+		$('.popUpReg').css('display', 'none');
+	});
+	$('#close').hover(function(){
+		$('#close').css("background","Silver");
+	},function(){
+		$('#close').css("background","white");
+	})
+	$('#newReg').hover(function(){
+		$('#newReg').css("background","Silver");
+	},function(){
+		$('#newReg').css("background","white");
+	})
+	$('#login').hover(function(){
+		$('#login').css("background","Silver");
+	},function(){
+		$('#login').css("background","white");
+	})
+	
+
+})
+
+function greyOut(){
+	if(document.getElementById('user').value==="" ||
+	document.getElementById('pass').value==="" ){
+		document.getElementById("newReg").disabled=true;
+	}
+	else{
+		document.getElementById("newReg").disabled=false;
+	}
+
+
+}
+
+
